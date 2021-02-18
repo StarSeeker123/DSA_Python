fe53643185b02e3d8719127b2172ca4304aa65cf
{Your-name} <{your-email-id}>
2021-02-18 23:05:49 +0530

    Update SubsetSum.py

--- Data Structures/Dynamic Programming/SubsetSum.py
+++ Data Structures/Dynamic Programming/SubsetSum.py
@@ -8,22 +8,34 @@
 
 #Playlist Link: https: // www.youtube.com/watch?v = nqowU...​''
 
-def subsetExists(val,W,n):
-    sum = 0
-    if(n==0):
-        return 0
-    
-    sum = subsetExists(val[n-1]+subsetExists(val,W,n-1))
-    
-    
+def isSubetSum(set_1,n,sum):
+    subset = ([[False for i in range(sum + 1)] for i in range(n + 1)])
+    # If sum is 0, then answer is true
+    for i in range(n + 1):
+        subset[i][0] = True
 
+    # If sum is not 0 and set is empty,
+    # then answer is false
 
+    for i in range(1, sum + 1):
+        subset[0][i] = False
+    for i in range(1, n + 1):
+        for j in range(1, sum + 1):
+            if j<set[i-1]:
+                subset[i][j] = subset[i-1][j]
+            if j>= set[i-1]:
+                subset[i][j] = (subset[i-1][j] or
+                                subset[i - 1][j-set[i-1]])
+    return subset[n][sum]
+
+    
+    
 
 #Driver code
-
-val = [3,34,4,12,5,2]
-W = 9
-n = len(val)
-print(subsetExists)
-
-
+set_1 = [3, 34, 4, 12, 5, 2]
+sum = 9
+n = len(set_1)
+if (isSubsetSum(set_1, n, sum) == True):
+    print("Found a subset with given sum")
+else:
+    print("No subset with given sum")
